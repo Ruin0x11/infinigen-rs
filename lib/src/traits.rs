e641df93a9cb5afb43b15be1caa1edd337940378
ruin <ipickering2@gmail.com>
2017-04-24 23:24:28 -0500

    Document things

--- lib/src/traits.rs
+++ lib/src/traits.rs
@@ -7,11 +7,14 @@ use serde::de::Deserialize;
 use managed_region::ManagedRegion;
 use region::*;
 
+/// A two-dimensional index into a grid, like those of chunks or regions.
 pub trait Index: Hash + Eq + PartialEq + Clone {
     fn x(&self) -> i32;
     fn y(&self) -> i32;
 }
 
+/// Describes a struct that can load and unload parts of the world. Typically
+/// used alongside a Manager for keeping track of unsaved chunks.
 pub trait Chunked<'a, H, I, C, R>
     where I:Index,
           C: Serialize + Deserialize,
@@ -25,6 +28,9 @@ pub trait Chunked<'a, H, I, C, R>
 
     fn save(self) -> SerialResult<()>;
 }
+
+/// Describes a struct that is responsible for keeping track of multiple
+/// ManagedRegions and retrieving the correct region for a given chunk index.
 pub trait Manager<'a, C, H, I, R>
     where I:Index,
           C: Serialize + Deserialize,
@@ -34,7 +40,7 @@ pub trait Manager<'a, C, H, I, R>
     fn load(&self, index: RegionIndex) -> R;
     fn get_for_chunk(&mut self, chunk_index: &I) -> &mut R;
     fn prune_empty(&mut self);
-    
+
     fn notify_chunk_creation(&mut self, chunk_index: &I) {
         let region = self.get_for_chunk(chunk_index);
         region.receive_created_chunk(chunk_index);
